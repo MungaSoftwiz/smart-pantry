b07c9d0a8dee41fbbee71c44b218da35ea87332f
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-05 01:59:12 +0300

    refactor: useState to handle state

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,45 @@ import {db} from "./firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { Container, Typography, List, ListItem, ListItemText, Button } from "@mui/material";
 import Link from "next/link";
-import AuthPage from "./auth/page";
+import { auth } from "./firebase"; // Import auth from firebase
+import { onAuthStateChanged } from "firebase/auth"; // Import onAuthStateChanged
+import AddItem from "./add/page";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [items, setItems] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
-    const fetchItems = async () => {
-      const querySnapshot = await getDocs(collection(db, "pantry-items"));
-      const itemsList = querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setItems(itemsList);
-    };
+    // Authentication listener
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+        router.push('/auth');
+      }
+    });
 
-    fetchItems();
+    // Clean up subscription on unmount
+    return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const fetchItems = async () => {
+        const querySnapshot = await getDocs(collection(db, "pantry-items"));
+        const itemsList = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setItems(itemsList);
+      };
+
+      fetchItems();
+    }
+  }, [isLoggedIn]);
 
   const handleDelete = (id) => {
     setItems(items.filter(item => item.id !== id));
@@ -29,7 +50,7 @@ export default function Home() {
 
   return (
     <Container>
-      <AuthPage setIsLoggedIn={setIsLoggedIn} />
+      <AddItem isLoggedIn={isLoggedIn} />
       {isLoggedIn ? (
         <>
           <Typography variant="h4" sx={{ marginTop: 2, color: "#4E342E" }}>Pantry Items:</Typography>
